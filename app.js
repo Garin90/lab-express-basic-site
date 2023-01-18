d54624eb98a734cb7a5673ef9c38160cdbd23524
Alex Garrido Baldrich <alex.garrido.baldrich@gmail.com>
2023-01-18 12:49:17 +0100

    Finished

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require ('express');
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Application running at port: ${port}`)
+})
+
+app.get('/', (req, res, next) => {
+    console.log("I'm in home");
+    res.render("home");
+})
+
+app.get('/about', (req, res, next) => {
+    console.log("I'm in about");
+    res.render("about");
+})
+
+app.get('/works', (req, res, next) => {
+    console.log("I'm in works");
+    res.render("works");
+})
+
+app.get('/gallery', (req,res,next) => {
+    console.log("I'm in gallery");
+    res.render("gallery");
+})
+
+
+
+
